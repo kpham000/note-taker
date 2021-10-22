698a213198db20062c08d80fdd3babea12aa8b27
kpham000 <90490117+kpham000@users.noreply.github.com>
2021-10-21 23:16:47 -0700

    reaad

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const noteRouter = require('./db/db.json');
 const app = express();
+var uniquid = require('uniquid')
 
 const PORT = 3000;
 
@@ -10,11 +11,17 @@ app.use(express.json());
 app use(express.urlencoded({extended: true}));
 
 app.get('/notes',(req,res) => 
-res.sendFile(path.join(__dirname, 'public/notes.html'))
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 app.get('/notes',(req,res) => 
-res.json(notes));
+    res.json(notes)
+);
+
+app.post('/api/notes', ((req, res)=>{
+    req.body.id = uniqid();
+    notes.push(req.body); })
+);
 
 app.listen(PORT, () =>
     console.log(`example app listen at http://localhost:${PORT}`)
-;)
+);
